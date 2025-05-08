54c8a35f711fa291c4cd2764fcd31b6648765058
pciturri <pciturri@gfz-potsdam.de>
2025-05-08 08:50:27 +0200

    added alaska, cascadia, japan catalogs

--- slow_py/catalogs.py
+++ slow_py/catalogs.py
@@ -86,6 +86,49 @@ def get_japan_catalog(depth=None, savepath=None):
         shp_name = os.path.join(basepath,
                                 '../data/poly4seismicity/FinalPolygons_full/japan.shp')
 
+    cat_fname = '../data/Japan_EQ_all.csv'
+    catalog = pandas.read_csv(cat_fname)
+
+    # catalog = csep.load_catalog(cat_fname)
+
+    point_geometries = [Point(lon, lat) for lon, lat in zip(catalog.longitude,
+                                                            catalog.latitude)]
+    points_gdf = gpd.GeoDataFrame(geometry=point_geometries, crs="EPSG:4326")
+    region = gpd.read_file(shp_name)
+
+    within_mask = points_gdf.within(region.union_all())
+
+    new_catalog = catalog[within_mask]
+
+    lons = new_catalog.longitude
+    lats = new_catalog.latitude
+    # csep.utils.time_utils.strptime_to_utc_epoch(catalog.datetime_utc[0])
+    epoch_times = [csep.utils.time_utils.strptime_to_utc_epoch(i) for i in new_catalog.datetime_utc]
+    depth = new_catalog.depth_km
+    magnitude = new_catalog.magnitude
+
+    events = []
+    for i, (lon, lat, dt, depth, mag) in enumerate(zip(lons, lats, epoch_times, depth, magnitude)):
+        events.append((i, dt, lat, lon, depth, mag))
+
+    new_catalog = csep.core.catalogs.CSEPCatalog(data=events)
+    if savepath:
+        new_catalog.write_ascii(savepath)
+    return new_catalog
+
+
+def get_japan_filtered_catalog(depth=None, savepath=None):
+
+    if depth == '70km' or depth == 70:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_70km/japan.shp')
+    elif depth == '150km' or depth == 150:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_150km/japan.shp')
+    else:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_full/japan.shp')
+
     cat_fname = '../data/Japan_EQ_filtered.csv'
     catalog = pandas.read_csv(cat_fname)
 
@@ -111,9 +154,91 @@ def get_japan_catalog(depth=None, savepath=None):
     for i, (lon, lat, dt, depth, mag) in enumerate(zip(lons, lats, epoch_times, depth, magnitude)):
         events.append((i, dt, lat, lon, depth, mag))
 
-    # print(events)
-    #
-    # return events
+    new_catalog = csep.core.catalogs.CSEPCatalog(data=events)
+    if savepath:
+        new_catalog.write_ascii(savepath)
+    return new_catalog
+
+
+def get_cascadia_catalog(depth=None, savepath=None):
+
+    if depth == '70km' or depth == 70:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_70km/cas.shp')
+    elif depth == '150km' or depth == 150:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_150km/cas.shp')
+    else:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_full/cas.shp')
+
+    cat_fname = '../data/Cascadia_EQ_all.csv'
+    catalog = pandas.read_csv(cat_fname)
+
+    # catalog = csep.load_catalog(cat_fname)
+
+    point_geometries = [Point(lon, lat) for lon, lat in zip(catalog.longitude,
+                                                            catalog.latitude)]
+    points_gdf = gpd.GeoDataFrame(geometry=point_geometries, crs="EPSG:4326")
+    region = gpd.read_file(shp_name)
+
+    within_mask = points_gdf.within(region.union_all())
+
+    new_catalog = catalog[within_mask]
+
+    lons = new_catalog.longitude
+    lats = new_catalog.latitude
+    # csep.utils.time_utils.strptime_to_utc_epoch(catalog.datetime_utc[0])
+    epoch_times = [csep.utils.time_utils.strptime_to_utc_epoch(i.replace('T', ' ').replace('Z', '')) for i in new_catalog.time]
+    depth = new_catalog.depth
+    magnitude = new_catalog.mag
+
+    events = []
+    for i, (lon, lat, dt, depth, mag) in enumerate(zip(lons, lats, epoch_times, depth, magnitude)):
+        events.append((i, dt, lat, lon, depth, mag))
+
+    new_catalog = csep.core.catalogs.CSEPCatalog(data=events)
+    if savepath:
+        new_catalog.write_ascii(savepath)
+    return new_catalog
+
+def get_alaska_catalog(depth=None, savepath=None):
+
+    if depth == '70km' or depth == 70:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_70km/alu.shp')
+    elif depth == '150km' or depth == 150:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_150km/alu.shp')
+    else:
+        shp_name = os.path.join(basepath,
+                                '../data/poly4seismicity/FinalPolygons_full/alu.shp')
+
+    cat_fname = '../data/Alaska_EQ_all.csv'
+    catalog = pandas.read_csv(cat_fname)
+
+    # catalog = csep.load_catalog(cat_fname)
+
+    point_geometries = [Point(lon, lat) for lon, lat in zip(catalog.longitude,
+                                                            catalog.latitude)]
+    points_gdf = gpd.GeoDataFrame(geometry=point_geometries, crs="EPSG:4326")
+    region = gpd.read_file(shp_name)
+
+    within_mask = points_gdf.within(region.union_all())
+
+    new_catalog = catalog[within_mask]
+
+    lons = new_catalog.longitude
+    lats = new_catalog.latitude
+    # csep.utils.time_utils.strptime_to_utc_epoch(catalog.datetime_utc[0])
+    epoch_times = [csep.utils.time_utils.strptime_to_utc_epoch(i.replace('T', ' ').replace('Z', '')) for i in new_catalog.time]
+    depth = new_catalog.depth
+    magnitude = new_catalog.mag
+
+    events = []
+    for i, (lon, lat, dt, depth, mag) in enumerate(zip(lons, lats, epoch_times, depth, magnitude)):
+        events.append((i, dt, lat, lon, depth, mag))
+
     new_catalog = csep.core.catalogs.CSEPCatalog(data=events)
     if savepath:
         new_catalog.write_ascii(savepath)
@@ -122,7 +247,10 @@ def get_japan_catalog(depth=None, savepath=None):
 FUNC_MAP = {
     # 'chile': get_chile_catalog,
     # 'mexico': get_mexico_catalog,
-    'japan': get_japan_catalog
+    # 'japan': get_japan_catalog,
+    # 'japan_filtered': get_japan_filtered_catalog,
+    'cascadia': get_cascadia_catalog
+
 }
 
 if __name__ == '__main__':
